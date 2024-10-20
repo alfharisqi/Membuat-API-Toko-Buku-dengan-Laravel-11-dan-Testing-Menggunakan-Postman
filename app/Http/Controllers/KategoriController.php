8fff3eecbc1128e3638bf8947f96882dc12b1a8b
alfhaberdasi <risqiwicaksonoalfha.com>
2024-10-20 14:32:13 +0700

    Menyelesaikan Pratikum

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+
+    public function index()
+    {
+        return Kategori::all();
+    }
+
+    public function store(Request $request)
+    {
+
+        try {
+            $request->validate(['nama_kategori' => 'required|unique:kategoris']);
+            $kategori = Kategori::create($request->all());
+            return response()->json($kategori, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
